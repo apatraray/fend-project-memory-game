45c2dcfe8c8d7c53541affdc47c9fe27f5b167f3
anita <anita.ani2007@gmail.com>
2018-05-26 02:19:06 -0700

    add timer function

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const cards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o
               'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt',
               'fa-cube', 'fa-cube', 'fa-anchor', 'fa-anchor',
               'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle'];
+const timer = document.querySelector('.timer');
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -43,7 +44,7 @@ let NumberOfMoves = 0;
 let cardMatchedPairPerMoves = 0;
 restart.addEventListener('click', restartGame);
 allCardList.forEach(function(card){
-    card.addEventListener('click', showCard);
+    card.addEventListener('click', showCard, true);
 });
 function restartGame(){
     NumberOfMoves = 0;
@@ -83,7 +84,7 @@ function showCard(event){
 //if clicked same card twice
 //star should display correctly
 //Timer should start when player starts a game
-
+//add responsiveness
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -94,3 +95,27 @@ function showCard(event){
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ let startSec = 0;
+ let totalSec = 0;
+ let startMin = 0;
+ let elapsedString = " ";
+ let x = setInterval(function(){
+   startSec++;
+   totalSec++;
+   if(startSec > 60){
+      startMin++;
+      startSec = 0;
+   }
+   if(startSec > 9) {
+     elapsedString = "0"+startMin+":"+startSec;
+   }
+   else {
+     elapsedString = "0"+startMin+":"+"0"+startSec;
+   }
+   timer.innerHTML = elapsedString;
+   if (totalSec > 300) {
+     clearInterval(x);
+     timer.innerHTML = '00:00';
+     //Display modal game over.
+   }
+ }, 1000);
